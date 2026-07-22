6c854696e1ca372cd817076cfc048a2d0b0d9953
HarryHelloo <hiharry-mdq@foxmail.com>
2026-07-22 13:08:19 +0800

    feat: 添加 ElCard 组件的默认属性设置以统一阴影效果

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
+import ElementPlus, { ElCard } from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
@@ -9,6 +9,10 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
 import '@/scss/global.scss'
+
+ElCard.setPropsDefaults({
+  shadow: "always",
+})
 
 const app = createApp(App)
 
